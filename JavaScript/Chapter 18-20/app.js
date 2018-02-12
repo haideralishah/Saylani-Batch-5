79274524c25cab74cc0e56517df6b3296f946d47
header <headeralishah@gmail.com>
2018-02-12 12:54:28 +0500

    same commit as last

--- JavaScript/Chapter 18-20/app.js
+++ JavaScript/Chapter 18-20/app.js
@@ -111,3 +111,10 @@
 // console.log(largestNumber);
 
 
+for(var i = 0 ; i <= 100; i++){
+    if(i % 2 === 0 ){
+        console.log(i + ' is even');
+    }else{
+        console.log(i + ' is odd');
+    }
+}
