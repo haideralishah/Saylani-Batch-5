da920814c3f2bef2af536b5bccbdd00d17070dff
kkp-321 <muhammadkhurramkhan68@gmail.com>
2018-03-28 12:51:39 +0500

    code till 28 March 2018

--- JavaScript/Chapter 69 to/app.js
+++ JavaScript/Chapter 69 to/app.js
@@ -24,16 +24,17 @@
 //         size: 'medium',
 //         frequency: '12000MH',
 //         quantity: 2
-//     }
-
-// gadgets: ['ac', 'deck', 'lcd', 'nos']
+//     },
+//     gadgets: ['ac', 'deck', 'lcd', 'nos']
 // }
+
+
 // console.log(carDetails)
 
-// // carDetails.ac = false
-// // console.log(carDetails)
+// carDetails.ac = false
+// console.log(carDetails)
 // // carDetails.gadgets = ['ac', 'deck', 'lcd', 'nos'];
-// // console.log(carDetails.gadgets[2])
+// console.log(carDetails.gadgets[2])
 
 
 // console.log(carDetails.speakrs.frequency)
@@ -113,7 +114,7 @@
 // var cities;
 
 
-// cities = ['karachi','lahore'];
+// cities = ['karachi', 'lahore'];
 
 
 
@@ -255,30 +256,28 @@
 /* Book Example 71*/
 
 
-
-var plan1 = {
-    name: "Basic",
-    price: 3.99,
-    space: 100,
-    transfer: 1000,
-    pages: 10,
-    discountMonths: [2, 7],
-    calcAnnual: function (prcentDis) {
-        var bestPrice = plan1.price;
-        var currDate = new Date();
-        var thisMo = currDate.getMonth();
-        for (var i = 0; i < plan1.discountMonths.length; i++) {
-            if (plan1.discountMonths[i] === thisMo) {
-                bestPrice = plan1.price * prcentDis;
-                break;
-            }
-        }
-        return bestPrice * 12;
-    }
-};
-
-var discountedValue = plan1.calcAnnual(.8)
-console.log(discountedValue);
+// var plan1 = {    
+//     name: "Basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10,
+//     discountMonths: [2, 7],
+//     calcAnnual: function (prcentDis) {
+//         var bestPrice = plan1.price;
+//         var currDate = new Date();
+//         var thisMo = currDate.getMonth();
+//         for (var i = 0; i < plan1.discountMonths.length; i++) {
+//             if (plan1.discountMonths[i] === thisMo) {
+//                 bestPrice = plan1.price * prcentDis;
+//                 break;
+//             }
+//         }
+//         return bestPrice * 12;
+//     }
+// };
+// var discountedValue = plan1.calcAnnual(.8)
+// console.log(discountedValue);
 
 
 
@@ -293,3 +292,160 @@ console.log(discountedValue);
 
 
 
+
+
+// var plan1 = {
+//     name: "Basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10,
+//     discountMonths: [2, 7],
+//     calcAnnual: function (prcentDis) {        
+//         var bestPrice = this.price;
+//         var currDate = new Date();
+//         var thisMo = currDate.getMonth();
+//         for (var i = 0; i < this.discountMonths.length; i++) {
+//             if (this.discountMonths[i] === thisMo) {
+//                 bestPrice = this.price * prcentDis;
+//                 break;
+//             }
+//         }
+//         return bestPrice * 12;
+//     }
+// };
+// var discountedValue = plan1.calcAnnual(.9)
+// console.log(discountedValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Plan(monthly, disSpace, dataTransfer, sitePages) {
+//     this.monthly = monthly;
+//     this.disSpace = disSpace;
+//     this.dataTransfer = dataTransfer;
+//     this.sitePages = sitePages;
+//     this.userName = 'haider';
+//     this.calcAnnual = function (prcentDis) { }
+
+// }
+
+// var price1 = new Plan(3.99, 100, 1000, 10);
+// var price2 = new Plan(5.99, 500, 5000, 50);
+// var price3 = new Plan(9.99, 2000, 20000, 500);
+
+
+// console.log(price1);
+// console.log(price2);
+// console.log(price3);
+
+
+
+
+
+
+
+
+// function Plan(monthly, disSpace, dataTransfer, sitePages) {
+//     this.monthly = monthly;
+//     this.disSpace = disSpace;
+//     this.dataTransfer = dataTransfer;
+//     this.sitePages = sitePages;
+// }
+
+// Plan.prototype.userName = 'haider';
+// Plan.prototype.calcAnnual = function () {
+//     console.log('haider');
+// }
+
+// var price1 = new Plan(3.99, 100, 1000, 10);
+// var price2 = new Plan(5.99, 500, 5000, 50);
+// var price3 = new Plan(9.99, 2000, 20000, 500);
+// price1.userName = 'Ali';
+
+
+// // price1.calcAnnual()
+// console.log(price1.userName);
+// console.log(price2);
+// console.log(price3);
+
+
+
+
+
+
+
+
+
+
+
+
+// var user = {
+//     userName: 'haider',
+//     roll: 2643,
+//     cell: '0341'    
+// }
+
+// // user.userName
+// // user['userName']
+
+// for (var key in user) {                
+//     console.log(user[key]);
+//     if(key === 'roll'){
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Plan(monthly, disSpace, dataTransfer, sitePages) {
+//     this.monthly = monthly;
+//     this.disSpace = disSpace;
+//     this.dataTransfer = dataTransfer;
+//     this.sitePages = sitePages;
+// }
+
+// Plan.prototype.userName = 'haider';
+// Plan.prototype.calcAnnual = function () {
+//     console.log('haider');
+// }
+
+// var price1 = new Plan(3.99, 100, 1000, 10);
+// var price2 = new Plan(5.99, 500, 5000, 50);
+// var price3 = new Plan(9.99, 2000, 20000, 500);
+// // hasOwnProperty(propName)
+// var ownProps = [];
+// for(var prop in price1){
+//     if(price1.hasOwnProperty(prop)){
+//         ownProps.push(prop);
+
+//     }
+// }
+
+// console.log(ownProps);
