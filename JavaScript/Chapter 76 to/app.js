236a95e2b904af74cd9d841dca44055ab046c52b
kkp-321 <muhammadkhurramkhan68@gmail.com>
2018-04-02 11:07:34 +0500

    class code till 30th March 2018

---
+++ JavaScript/Chapter 76 to/app.js
@@ -0,0 +1,36 @@
+// variable for std name
+var stdName = document.getElementById('name');
+// variable for DOB
+var DOB = document.getElementById('DOB');
+// variable for roll no
+var rollNo = document.getElementById('roll');
+// variable for error element
+var errorEl = document.getElementById('error');
+// array for all users
+var userArr;
+
+function getInput() {
+    if (stdName.value !== '' && DOB.value !== '' && rollNo.value !== '') {
+        // create user object
+        var user = new GenerateObject(stdName.value, DOB.value, rollNo.value);
+        // push new user to existing array
+        var localStorageData = localStorage.getItem('users')
+        if (localStorageData !== null) {
+            userArr = JSON.parse(localStorageData);
+        }
+        else {
+            userArr = [];
+        }
+        userArr.push(user);
+        localStorage.setItem('users', JSON.stringify(userArr));
+    } else {
+        // error if fields are empty
+        errorEl.innerHTML = 'Please fill all the fields';
+    }
+}
+// constructor function
+function GenerateObject(stdName, DOB, roll) {
+    this.studentName = stdName;
+    this.dateOfBirth = DOB;
+    this.rollNo = roll;
+}
