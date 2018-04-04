e9945634b600a2de321660f4cb6097d7dd0fde74
kkp-321 <muhammadkhurramkhan68@gmail.com>
2018-04-04 12:46:56 +0500

    Class code till April 4th, 2018

---
+++ JavaScript/Chapter 79 to 81/app.js
@@ -0,0 +1,168 @@
+// var whereUserCameFrom = document.referrer;
+
+
+// console.log(whereUserCameFrom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function signin() {   
+//     var monkeyWindow = window.open();
+//     var signInText = "<h1>G+ Sign in Monkey Window</h1>";
+//     monkeyWindow.document.write(signInText);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// function signin() {
+//     var monkeyWindow = window.open();
+//     // monkeyWindow.location.href='https://www.facebook.com';
+//     monkeyWindow.location.assign('https://www.facebook.com');
+
+// }
+
+
+
+
+
+
+
+// function signin() {
+//     var monkeyWindow = window.open('https://www.facebook.com');  
+// }
+
+
+
+
+
+
+
+
+// function signin() {
+//     var monkeyWindow = window.open('about.html');  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function signin() {
+//     var monkeyWindow = window.open('about.html', 'aboute page', 'height=600,width=600,top=250,left=300');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkForPopBlocker() {
+//     var testPop = window.open("", "", "width=100,height=100");
+//     if (testPop === null || typeof(testPop === "undefined")) {
+//         alert("Please disable your popup blocker.");
+//     }
+//     // else{
+//         testPop.close();
+//     // }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var answers = {
+//     answer1: 'apple',
+//     answer2: 'banana',
+//     answer3: 'orange',
+//     answer4: 'grapes'
+// }
+
+
+
+// 1-4
+// var randomeNumbers = [];
+// Generate 4 random numbers
+
+
+//console.log(randomeNumbers);
