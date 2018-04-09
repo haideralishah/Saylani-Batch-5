a61c94390c2c21f9c5018ed5e3e64630a4f31142
kkp-321 <muhammadkhurramkhan68@gmail.com>
2018-04-09 11:51:03 +0500

    second task with chapter 82. Color of form field changing as it is blinking.

--- JavaScript/Chapters 82/app.js
+++ JavaScript/Chapters 82/app.js
@@ -1,3 +1,26 @@
+// function checkForLastName() {
+//     console.log('hello world');
+//     var targetNodeEl = document.getElementById('lastNameField');
+//     if (targetNodeEl.value.length === 0) {
+//         console.log('Last name is required!');
+//         targetNodeEl.focus();
+//         targetNodeEl.style.backgroundColor = 'yellow';
+//         setTimeout(function () {
+//             targetNodeEl.style.backgroundColor = 'white';
+//         }, 200);
+//     }
+//     return false;
+// }
+
+
+
+
+
+
+
+
+
+
 function checkForLastName() {
     console.log('hello world');
     var targetNodeEl = document.getElementById('lastNameField');
@@ -5,9 +28,20 @@ function checkForLastName() {
         console.log('Last name is required!');
         targetNodeEl.focus();
         targetNodeEl.style.backgroundColor = 'yellow';
-        setTimeout(function () {
-            targetNodeEl.style.backgroundColor = 'white';
-        }, 200);
+        var count = 0;
+        var changeFormFieldColor = setInterval(function () {
+            if (count % 2 === 0) {
+                targetNodeEl.style.backgroundColor = 'yellow';
+            }
+            else {
+                targetNodeEl.style.backgroundColor = 'white';
+            }
+            count++;
+            if(count === 6){
+                clearInterval(changeFormFieldColor);
+                count = 0;
+            }
+        }, 100);
     }
     return false;
 }
